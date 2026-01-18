83ed0a710c311ed1b294ff06bbe8059fafe6fdad
Wedding Invitation <user@invitation.local>
2026-01-19 02:23:16 +0900

    fix: bride position 0%

--- src/App.jsx
+++ src/App.jsx
@@ -1425,7 +1425,7 @@ END:VCALENDAR`;
                     width: '100%',
                     height: '160%',
                     objectFit: 'cover',
-                    objectPosition: '10% 25%',
+                    objectPosition: '0% 25%',
                     position: 'absolute',
                     top: '-30%',
                     transform: 'scale(1.2)',
